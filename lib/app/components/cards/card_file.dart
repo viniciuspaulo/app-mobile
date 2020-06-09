e70e568f7065cda390ba0f73f1f3d35c08a5d399
vinicius dos santos brito <vbbritodepaulo@gmail.com>
2020-06-09 16:02:52 -0300

    Download documento e compartilhar arquivo

--- lib/app/components/cards/card_file.dart
+++ lib/app/components/cards/card_file.dart
@@ -4,50 +4,111 @@ import 'dart:typed_data';
 
 import 'package:Clinicarx/app/models/AttendancesModel.dart';
 import 'package:Clinicarx/app/repositories/AttendanceRepository.dart';
+import 'package:downloads_path_provider/downloads_path_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:Clinicarx/app/modules/home/attendance/attendance_doc_screen.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:share/share.dart';
+import 'package:share_extend/share_extend.dart';
 import 'package:toast/toast.dart';
 
-class CardFile extends StatelessWidget {
+class CardFile extends StatefulWidget {
   final AttendancesFilesModel attendenceFile;
   CardFile(this.attendenceFile);
 
   @override
+  _CardFile createState() => _CardFile();
+}
+
+class _CardFile extends State<CardFile> {
+  bool load = false;
+
+  @override
+  initState() {
+    super.initState();
+  }
+
+  Future<File> saveFile() async {
+    setState(() => load = true);
+    var status = await Permission.storage.status;
+    if (!status.isGranted) {
+      await Permission.storage.request();
+    }
+
+    final repositorio = Modular.get<AttendanceRepository>();
+    var base64 =
+        await repositorio.getAttendenceFileDetail(widget.attendenceFile);
+
+    try {
+      Uint8List bytes = base64Decode(base64);
+      Directory downloadsDirectory =
+          (await DownloadsPathProvider.downloadsDirectory);
+
+      File file = new File(
+          downloadsDirectory.path + '/' + widget.attendenceFile.id + ".pdf");
+
+      await file.writeAsBytes(bytes.buffer.asUint8List());
+
+      setState(() => load = false);
+      return file;
+    } catch (e) {
+      setState(() => load = false);
+      print(e);
+      Toast.show(
+          "Não foi possível salvar este pdf, verifique se você deu permissão.",
+          context,
+          duration: Toast.LENGTH_LONG);
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: () async {
-        Toast.show("Este pdf foi salvo no seu dispositivo.", context,
-            duration: Toast.LENGTH_LONG);
+    return ListTile(
+      leading: Icon(FontAwesomeIcons.fileAlt, color: Colors.teal),
+      title: InkWell(
+        onTap: () async {
+          try {
+            File file = await saveFile();
+            Toast.show("Pdf salvo nos downloads.", context);
 
-        final repositorio = Modular.get<AttendanceRepository>();
-        var base64 = await repositorio.getAttendenceFileDetail(attendenceFile);
-
-        try {
-          Uint8List bytes = base64Decode(base64);
-          String dir = (await getApplicationDocumentsDirectory()).path;
-          File file = new File('$dir/' + attendenceFile.id + ".pdf");
-          await file.writeAsBytes(bytes.buffer.asUint8List());
-
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-                builder: (context) => AttendanceDocScreen(file.path)),
-          );
-        } catch (e) {
-          Toast.show("Não foi possível exibir este pdf.", context,
-              duration: Toast.LENGTH_LONG);
-        }
-      },
-      child: ListTile(
-        leading: Icon(FontAwesomeIcons.fileAlt, color: Colors.teal),
-        title: Text(
-          attendenceFile.name,
+            Future.delayed(Duration(seconds: 3), () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) =>
+                        AttendanceDocScreen(file.path, widget.attendenceFile)),
+              );
+            });
+          } catch (e) {}
+        },
+        child: Text(
+          widget.attendenceFile.name,
           style: TextStyle(color: Colors.teal),
         ),
       ),
+      trailing: this.load
+          ? SizedBox(
+              width: 17,
+              height: 17,
+              child: CircularProgressIndicator(
+                backgroundColor: Colors.white,
+                strokeWidth: 1,
+              ),
+            )
+          : InkWell(
+              child: Icon(FontAwesomeIcons.shareAlt),
+              onTap: () async {
+                try {
+                  File file = await saveFile();
+                  ShareExtend.share(file.path, "Clinicarx");
+                } catch (e) {
+                  print(e);
+                }
+              },
+            ),
     );
   }
 }
--- lib/app/modules/home/attendance/attendance_doc_screen.dart
+++ lib/app/modules/home/attendance/attendance_doc_screen.dart
@@ -1,52 +1,34 @@
-
+import 'package:Clinicarx/app/models/AttendancesModel.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_full_pdf_viewer/full_pdf_viewer_scaffold.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:share/share.dart';
-import 'package:toast/toast.dart';
 
-
-class AttendanceDocScreen extends StatefulWidget{
-
+class AttendanceDocScreen extends StatefulWidget {
+  final AttendancesFilesModel attendenceFile;
   final String pathPDF;
-  AttendanceDocScreen(this.pathPDF);
+  AttendanceDocScreen(this.pathPDF, this.attendenceFile);
 
   @override
   _AttendanceDocScreenState createState() => _AttendanceDocScreenState();
 }
 
 class _AttendanceDocScreenState extends State<AttendanceDocScreen> {
-
-  salvarArquivo() { 
-    Toast.show("Pdf salvo.", context);
-  }
-
   @override
   Widget build(BuildContext context) {
     return PDFViewerScaffold(
-      appBar: AppBar(
-        title: Image(
-          width: 120,
-          image: AssetImage("assets/images/logo.png",),
-        ),
-        elevation: 0,
-        backgroundColor: Colors.white,
-        iconTheme: IconThemeData(
-          color: Colors.black54
-        ),
-        actions: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: InkWell(
-              child: Icon(FontAwesomeIcons.shareAlt),
-              onTap: () {
-                Share.share(widget.pathPDF);
-              },
+        appBar: AppBar(
+            title: Center(
+              child: Image(
+                width: 120,
+                image: AssetImage(
+                  "assets/images/logo.png",
+                ),
+              ),
             ),
-          )
-        ],
-      ),
-      path: widget.pathPDF);
+            elevation: 0,
+            backgroundColor: Colors.white,
+            iconTheme: IconThemeData(color: Colors.black54)),
+        path: widget.pathPDF);
   }
 }
-
