8efe7338d827acf112cf27003d72f695480a229d
vinicius dos santos brito <vbbritodepaulo@gmail.com>
2020-06-09 16:03:39 -0300

    Download documento e compartilhar arquivo

--- lib/app/modules/home/attendance/attendance_doc_screen.dart
+++ lib/app/modules/home/attendance/attendance_doc_screen.dart
@@ -1,8 +1,6 @@
 import 'package:Clinicarx/app/models/AttendancesModel.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_full_pdf_viewer/full_pdf_viewer_scaffold.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:share/share.dart';
 
 class AttendanceDocScreen extends StatefulWidget {
   final AttendancesFilesModel attendenceFile;
